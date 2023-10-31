4a151e6f4b3f1d51fd45005922611410acc9e1e8
SabrinaLiu999 <60949285+SabrinaLiu999@users.noreply.github.com>
2023-10-30 20:00:04 -0700

    Update Code_smells_example.py

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,16 +1,19 @@
 #Code Smells Example for long method
+# calculate the total price of the item after discount
 def calculate_total_price(items):
     total = 0
     for item in items:
         if item.quantity > 0:
             total = total + discount(item.price)
     return total
-
+    
+# Get the unit price with discount
 def discount(price):
+    #Setting the baseline for discounts
     discontLine = 100
     if price > discontLine:
-        price = price * 0.9
+        price = price * 0.9 #10% off
         return price
     else:
-        price = price * 0.95
+        price = price * 0.95 #5% off
         return price
